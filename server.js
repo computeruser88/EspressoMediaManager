da6ad3c983389ea04f1a15bca6ab8fd3ff10e83a
Meghna <meghnanag16@gmail.com>
2018-05-19 14:04:35 -0700

    user view initial

--- server.js
+++ server.js
@@ -36,7 +36,13 @@ app.use(express.static("public"));
 require("./routes/user-api-routes.js")(app);
 
 require("./routes/admin-api-routes.js")(app);*/
-require("./routes/public-api-routes.js")(app);
+//require("./routes/public-api-routes.js")(app);
+
+
+ //require('./controllers/admin-api-routes.js')(app);
+ //require('./controllers/user-api-routes.js')(app);
+ //require('./controllers/public-api-routes.js')(app);
+
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
