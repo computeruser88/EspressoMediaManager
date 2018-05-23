621ca7ddba555cbf2abcf1c7cea9401250277e22
Hnin Haymar <hninhaymar@gmail.com>
2018-05-22 20:53:55 -0700

    adding admin-update-media API route

--- controllers/admin-api-routes.js
+++ controllers/admin-api-routes.js
@@ -9,6 +9,45 @@ module.exports = function(app) {
         });
     });
 
+    //admin-update-media API route needed - to allow admin to update media's quantity and time_limit
+    app.get("/admin-update-media/:mediaid/:quantity/:time_limit", function(req,res) {
+        var mediaId = req.params.mediaid;
+        var quantity = req.params.quantity;
+        var time_limit = req.params.time_limit;
+
+        db.Media.findOne({
+            where : {
+                id : mediaId
+            }
+        }).then(function(obj){
+            console.log(JSON.stringify(obj));
+            if(obj) {
+                console.log("Media exists. Let's update these quantity and time_limit fields");
+                if(quantity) {
+                    obj.update(
+                        {quantity : quantity}
+                    ).then(function(result) {
+                        console.log(JSON.stringify(result));
+                        console.log("quantity field updated");
+                    });
+                }
+
+                if(time_limit) {
+                    obj.update({ 
+                        time_limit: time_limit
+                    }).then(function(result) {
+                        console.log(JSON.stringify(result));
+                        console.log("time_limit field updated");
+                        res.json(result);
+                    });
+                }
+            }
+            else{
+                console.log("No media with this ID : " + mediaId + " found. Nothing to update");
+            }
+        });
+    });
+
     app.get("/admin-show-users", function(req,res) {
         db.User.findAll({
             where: { 
