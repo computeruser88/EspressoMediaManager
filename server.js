a1b5228c4b395170664c66f6ed66f7200ab77ca2
Meghna <meghnanag16@gmail.com>
2018-05-22 18:14:55 -0700

    resolving conflicts

--- server.js
+++ server.js
@@ -36,8 +36,6 @@ app.use(express.static("public"));
 
 
 
-
-
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 db.sequelize.sync({ force: true }).then(function() {
