5df976cb8afd82901bca8928cf5de94de7418fb7
Jan Balangue <jantonio.balangue@gmail.com>
2018-05-22 18:41:09 -0700

    Add navbar

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -53,7 +53,26 @@ $(document).ready(readyFunc);
 // delarations; no functions are actually executed
 
 function readyFunc() {
+      // Close mobile & tablet menu on item click
+  $('.navbar-item').each(function(e) {
+    $(this).click(function(){
+      if($('#navbar-burger-id').hasClass('is-active')){
+        $('#navbar-burger-id').removeClass('is-active');
+        $('#navbar-menu-id').removeClass('is-active');
+      }
+    });
+  });
 
+  // Open or Close mobile & tablet menu
+  $('#navbar-burger-id').click(function () {
+    if($('#navbar-burger-id').hasClass('is-active')){
+      $('#navbar-burger-id').removeClass('is-active');
+      $('#navbar-menu-id').removeClass('is-active');
+    }else {
+      $('#navbar-burger-id').addClass('is-active');
+      $('#navbar-menu-id').addClass('is-active');
+    }
+  });
     // click handler for the Operation radio buttons
     $('#opbuttons').on( "click", function() {
 
