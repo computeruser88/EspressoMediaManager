ce17fa4361327ac594a7667dc3b2b9492a4c8975
Hnin Haymar <hninhaymar@gmail.com>
2018-05-22 18:41:40 -0700

    user-dashboard api working, removed forced: true from db sync

--- controllers/user-api-routes.js
+++ controllers/user-api-routes.js
@@ -16,6 +16,26 @@ router.post('/user/new', function(req, res) {
     })
   })
   
+
+//[db.Media.name,'Media Name'], db.Transaction.checked_out_date, db.Media.genre, db.Media.type, [db.Media.cost,"cost per day"]
 module.exports = function(app) {
+  app.get("/user-dashboard/:email", function(req,res) {
+    //return all media in the transaction table for this user-email
+    var email = req.params.email;
+
+    db.Transaction.findAll({
+      attributes : [ 'checked_out_date', 'MediumId', 'returned_date' ] ,
+      where: { 
+          UserEmail : email
+      },
+      include: [{
+        model: db.Media
+      }]
+    }).then(function(dbMediaTransaction) {
+      res.json(dbMediaTransaction);
+    });
+
+  });
+
 
 };
