809174c94c9da829c9dd4fe66efe6cce7b57b681
Hnin Haymar <hninhaymar@gmail.com>
2018-05-26 15:03:00 -0700

    redirect after sign-up/login working

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -172,6 +172,7 @@ function readyFunc() {
 }
 
 function saveSignupData(name, email, password) {
+  console.log("saving signup for " + name + " email: " + email);
   var inputs = {};
   inputs.name = name;
   inputs.email = email;
@@ -181,8 +182,12 @@ function saveSignupData(name, email, password) {
     url: "/public/new-user/",
     data: inputs
   }).done(function (data) {
-    if (data.length === 1) {
-      console.log("authentication: success");
+    console.log("after sign up");
+    console.log(data);
+    if (data) {
+      console.log("signup: success");
+      authenticate(data.email,data.password);
+      
     } else {
       console.log("authentication: failure");
     }
@@ -195,13 +200,23 @@ function authenticate(email, password) {
   inputs.password = password;
   $.ajax({
     type: 'GET',
-    url: "/public/user-authenticate/",
+    url: "/public/user-authenticate/"+email+"/"+password,
     data: inputs
   }).done(function (data) {
-    if (data.length === 1) {
+    console.log("after login");
+    console.log(data);
+    if (data && data.length > 0) {
       console.log("authentication: success");
+      var currentUrl = window.location.href.split('/').pop();
+      console.log("currentUrl: " + currentUrl);
+      var targetUrl = currentUrl +"/user-view/" + data[0].email;
+      console.log(targetUrl);
+      window.location.replace(targetUrl);
+      
     } else {
       console.log("authentication: failure");
+      $("#login-modal").addClass("is-active");
+      $(".modal-card-title").html("Login");
     }
   });
 }
