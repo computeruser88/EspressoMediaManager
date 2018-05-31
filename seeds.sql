d55dfa93d6210b548e136a289b7c81e4d0fd0e30
Radhika <radhikaiyer@Radhikas-MacBook-Air.local>
2018-05-30 21:12:44 -0700

    Fixing seeds

--- seeds.sql
+++ seeds.sql
@@ -2,7 +2,7 @@ INSERT INTO media(name,type,genre,rating,year,quantity,time_limit,cost,createdAt
 VALUES("Beauty and the Beast","Movie","Family, Fantasy, Musical","G",2017,7,5,0.0,NOW(), NOW());
 
 INSERT INTO media(name,type,genre,rating,year,quantity,time_limit,cost,createdAt, updatedAt)
-VALUES("Dead Pool 2","Movie","Action, Adventure", "R",2018,10,3,0.0,NOW(), NOW());
+VALUES("Deadpool 2","Movie","Action, Adventure", "R",2018,10,3,0.0,NOW(), NOW());
 
 
 insert into users(name,email,password,createdAt,updatedAt) 
